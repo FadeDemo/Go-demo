73bc4f968381b0e2e4f6f31bf030f893aa628b87
paigeman <53284808+paigeman@users.noreply.github.com>
2024-08-12 21:11:51 +0800

    2024/08/12

---
+++ gopl/ch2/popcount/main.go
@@ -0,0 +1,33 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+
+// (Package doc comment intentionally malformed to demonstrate golint.)
+// !+
+package popcount
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		// pc[i/2]代表前n-1位有几个1，byte(i&1)判断最后一位是不是1
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+// 这个函数存在的必要性是x是uint64类型的数，而pc只能表示一个字节范围的数
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+//!-
---
+++ gopl/go.mod
@@ -0,0 +1,5 @@
+module github.com/FadeDemo/Go-demo/tree/main/gopl
+
+go 1.22.5
+
+require gopl.io v0.0.0-20211004154805-1ae3ec64947b // indirect
