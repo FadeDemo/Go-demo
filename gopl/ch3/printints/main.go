e102a2f914114cc68200ca2af48cc89cea12b919
paigeman <53284808+paigeman@users.noreply.github.com>
2024-08-15 20:51:22 +0800

    2024/08/15

---
+++ gopl/ch3/printints/main.go
@@ -0,0 +1,37 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 74.
+
+// Printints demonstrates the use of bytes.Buffer to format a string.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+// !+
+// intsToString is like fmt.Sprint(values) but adds commas.
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		_, err := fmt.Fprintf(&buf, "%d", v)
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3})) // "[1, 2, 3]"
+}
+
+//!-
---
+++ gopl/ch3/string/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "你好"
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Printf("%c\n", s[0])
+
+	a := 'a'
+	b := '你'
+	fmt.Printf("%c\n", a)
+	fmt.Printf("%c\n", b)
+	fmt.Printf("% x\n", s)
+	r := []rune(s)
+	fmt.Printf("%x\n", r)
+	r = []int32(s)
+	fmt.Printf("%x\n", r)
+}
