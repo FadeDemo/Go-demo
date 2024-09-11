8e4ebd698d8892d1f0483d4c970226070a61ec71
paigeman <53284808+paigeman@users.noreply.github.com>
2024-09-11 18:06:19 +0800

    2024/09/11

---
+++ gopl/ch9/exercises/9_1/bank.go
@@ -0,0 +1,41 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 261.
+//!+
+
+// Package bank provides a concurrency-safe bank with one account.
+package bank
+
+var deposits = make(chan int)  // send amount to deposit
+var balances = make(chan int)  // receive balance
+var withdraws = make(chan int) // send amount to withdraw
+
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+
+//func Withdraw(amount int) bool {
+//	if Balance() < amount {
+//		return false
+//	}
+//
+//}
+
+func teller() {
+	var balance int // balance is confined to teller goroutine
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		case amount := <-withdraws:
+			balance -= amount
+		}
+	}
+}
+
+func init() {
+	go teller() // start the monitor goroutine
+}
+
+//!-
